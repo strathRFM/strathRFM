c2f9353d802aa5b070bf1b8fb76bb490f9fe92d2
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2023-03-09 12:45:06 +0000

    updated local variables

--- rfsoc/class in test specCTRL/specCTRL.py
+++ rfsoc/class in test specCTRL/specCTRL.py
@@ -1,33 +1,39 @@
+#!/usr/local/ python3
+
+# coding: utf-8
+
 #import "spectrum.py"
 import pickle
 import time
 import numpy as np
 class SpecCTRL:
     def __init__(self,_config_Path = "config.pkl", # path to the configuration file writable by GUI
-                 _spec_Data_Stream_Path = "data.pkl", # path of data file writable from this calss
-                _status_file_path = ""):   # path of the file for specCTRL status for GUI
-        
+                 _spec_Data_Stream_Path = "data.pkl" ):  # path of data file writable from this calss
+                 
         self.config_Path = _config_Path
         self.spec_Data_Stream_Path = _spec_Data_Stream_Path
-        self.status_file_path = _status_file_path
         #self.spec = spectrum()
         self.start_on_boot = True
         self.continuous_scan_enable = True
         self.full_spectrum_scan = False # enable full spectrum scan
         self.stream_data_enable = False
-        self.data_file_ready = True    # if data file was read changed from GUI
-        self.data_file_written = False # indicate that the data file has been written
         self.app_enable = True         # continuously check if system needs to run.
                                        # if False exit specCTRL class and continue with boot.
-        self.fft_size = 4096
-        #self.centre_frequency = spec.centre_frequency
-        #self.decimation_factor = spec.decimation_factor
+        
+        # Spectrum class default values
+        self.fft_size = self.spec.get_fftsize()
+        self.centre_frequency = self.spec.get_centre_frequency()
+        self.decimation_factor = self.spec.get_decimation_factor()
+        self.units = self.spec.get_spectrum_units()
+        self.window = self.spec.get_window()
+        self.frame_number = self.spec.get_frames_number()
+        self.coordinates = self.spec.get_coordinates()
         
         
         
     def continuous_scan(self):
         # need to modify continuous scan to only progress if 
-        #self.spec.continuous_scan(prog)
+        #self.spec.continuous_scan()
         print("scanning continuously.")
        
     def unpickleFile(self, file_path):
@@ -63,10 +69,11 @@ class SpecCTRL:
         return res
         
     def create_config(self):
-        config_file = {b'continuous_scan_enable': True,
+        config_file = {b'continuous_scan_enable': False,
+                      b'changed':True
                       b'data_file_ready':True,
                       b'start_on_boot': False,
-                      b'full_spectrum_scan':True,
+                      b'full_spectrum_scan':False,
                       b'fft_size':4096,
                       b'app_enable':True,
                       b'stream_data_enable':False}
@@ -75,22 +82,22 @@ class SpecCTRL:
     def check_config(self):
         # this function loads in the config file and updates class variables
         res, config_file = self.unpickleFile(self.config_Path)
-       
+        if(config_file[b'changed'] == True):
             
-        self.continuous_scan_enable = config_file[b'continuous_scan_enable'] # or False
-        self.stream_data_enable = config_file[b'stream_data_enable']
-        self.data_file_ready = config_file[b'data_file_ready']
-        self.start_on_boot = config_file[b'start_on_boot']
-        self.app_enable = config_file[b'app_enable']
-        self.full_spectrum_scan = config_file[b'full_spectrum_scan']
-        # check time and update if discrepancy is detected
-        
-        # check other spectrum parameters
-        # if different than what is stored here, than update them in spectrum class
-        # and here
-        if(self.fft_size != config_file[b'fft_size']):
-            #self.spec.set_fft_size(file["fft_size"])
-            self.fft_size = config_file[b'fft_size']
+            self.continuous_scan_enable = config_file[b'continuous_scan_enable'] # or False
+            self.stream_data_enable = config_file[b'stream_data_enable']
+            self.data_file_ready = config_file[b'data_file_ready']
+            self.start_on_boot = config_file[b'start_on_boot']
+            self.app_enable = config_file[b'app_enable']
+            self.full_spectrum_scan = config_file[b'full_spectrum_scan']
+            # check time and update if discrepancy is detected
+
+            # check other spectrum parameters
+            # if different than what is stored here, than update them in spectrum class
+            # and here
+            if(self.fft_size != config_file[b'fft_size']):
+                #self.spec.set_fft_size(file["fft_size"])
+                self.fft_size = config_file[b'fft_size']
         
     
     def send_spec_data(self):
@@ -109,10 +116,8 @@ class SpecCTRL:
                 
         
     def start_CTRL(self):
+        self.create_config()
         while(self.app_enable):
-            self.print_file()
-            time.sleep(4)
-            self.check_config()
             if(self.continuous_scan_enable):
                 # background gather data for dataset
                 print("continuous scan enabled.")
@@ -121,7 +126,8 @@ class SpecCTRL:
                 # send data to GUI trough stream data file.
                 print("sending data to GUI.")
                 self.send_spec_data()
-            
+            #time.sleep(4)
+            self.check_config()
             
 ctrl = SpecCTRL()
 #ctrl.create_config()

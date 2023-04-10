ba1fc55e03a3fe93a4a925f9e6e5db4af1dcd469
eilidhrh <61511620+eilidhrh@users.noreply.github.com>
2023-04-10 15:34:31 +0100

    Analysis and rename file

---
+++ rfsoc/dataset/Troon spectrum data/renaming.py
@@ -0,0 +1,21 @@
+import os
+import glob
+
+directory = "C:\\Users\\eilid\\strathRFM\\rfsoc\\dataset\\Troon spectrum data\\"
+
+files = glob.glob(directory + "S_??-??-??_*.sigmf-meta")
+
+# Iterate through the list of files and rename them
+for file in files:
+    day = file.split("_")[1].split("-")[0]
+    month = file.split("_")[1].split("-")[1]
+    year = file.split("_")[1].split("-")[2]
+    tt = file.split("_")[2].split(".")[0]
+
+    new_filename = "S_{}-{}-{}_{}.sigmf-data".format(year, month, day, tt)
+
+    new_file_path = file.replace(os.path.basename(file), new_filename)
+
+    os.rename(file, new_file_path)
+
+    print("Renamed {} to {}".format(file, new_file_path))

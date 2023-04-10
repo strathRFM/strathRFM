8b59c388a4184f0ddcf66ea49732153419dbb5dd
eilidhrh <61511620+eilidhrh@users.noreply.github.com>
2023-04-10 15:44:07 +0100

    Fixed troon naming convention

--- rfsoc/dataset/renaming.py
+++ rfsoc/dataset/renaming.py
@@ -3,7 +3,7 @@ import glob
 
 directory = "C:\\Users\\eilid\\strathRFM\\rfsoc\\dataset\\Troon spectrum data\\"
 
-files = glob.glob(directory + "S_??-??-??_*.sigmf-meta")
+files = glob.glob(directory + "S_??-??-??_*.sigmf-data")
 
 # Iterate through the list of files and rename them
 for file in files:
@@ -12,7 +12,7 @@ for file in files:
     year = file.split("_")[1].split("-")[2]
     tt = file.split("_")[2].split(".")[0]
 
-    new_filename = "S_{}-{}-{}_{}.sigmf-meta".format(year, month, day, tt)
+    new_filename = "S_{}-{}-{}_{}.sigmf-data".format(year, month, day, tt)
 
     new_file_path = file.replace(os.path.basename(file), new_filename)
     if(file != new_file_path):

2d831d6f6470a1fb6d782c1676b30c531aca0e4f
Robert A Incze <98164670+Inczert@users.noreply.github.com>
2023-04-14 17:20:33 +0100

    Working Boot file
    Uses sub process and prints output to log.txt after execution.

--- rfsoc/Other/boot.py
+++ rfsoc/Other/boot.py
@@ -57,6 +57,7 @@
 from time import sleep
 from pynq.overlays.base import BaseOverlay
 import pickle
+import subprocess
 
 
 specCTRL_path = '/home/xilinx/jupyter_notebooks/strathRFM/specCTRL.py'
@@ -115,12 +116,7 @@ def blink8_alt():
 def walk8():
     base.leds[0:4].off()
     for _ in range(4):
-        for i in range(0,3):
-            base.leds[i].on()
-            sleep(0.1)
-            base.leds[i].off()
-            
-        for i in range(3,0):
+        for i in range(0,4):
             base.leds[i].on()
             sleep(0.1)
             base.leds[i].off()
@@ -155,23 +151,30 @@ if res:
         walk8()
         if f[b'continuous_scan_enable']:
             f[b'status'] = "continuous scan..."
-            pickleFile(f)
-            base.leds[0:4].off()
-            sleep(0.01)
-            base.leds[0:2].on()
         else:
             f[b'status'] = "idle mode..."
-            pickleFile(f)
-            base.leds[0:4].off()
-            sleep(0.01)
-            base.leds[0].on()
+        pickleFile(f)
+        base.leds[0:4].off()
         # initialise specCTRL
         try:
-            exec(open(specCTRL_path).read())
-            f[b'status'] = "specCTRL exited."
+            process = subprocess.Popen(['python', '/home/xilinx/jupyter_notebooks/strathRFM/specCTRL.py'], stdout=subprocess.PIPE, universal_newlines=True)
+            with open('/home/xilinx/jupyter_notebooks/strathRFM/log.txt', 'w') as fil:
+                while True:
+                    output = process.stdout.readline()
+                    fil.write(str(output.strip())+"\n")
+                    # Do something else
+                    return_code = process.poll()
+                    if return_code is not None:
+                        fil.write(str('RETURN CODE', return_code))
+                        # Process has finished, read rest of the output
+                        for output in process.stdout.readlines():
+                            fil.write(str(output.strip())+"\n")
+                        break
+        #os.system('cd /home/xilinx/jupyter_notebooks/strathRFM/')
+        #os.system('sudo python /home/xilinx/jupyter_notebooks/strathRFM/specCTRL.py')
         except:
             f[b'status'] = "specCTRL execution failed."
-        
+
         pickleFile(f)
         # reconfiguring overlay
         base = BaseOverlay('base.bit')
